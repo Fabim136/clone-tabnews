8c6c7d45b5df3f98168b2fe36cffd0ce1a27067e
fabim136 <102354096+Fabim136@users.noreply.github.com>
2025-01-27 12:44:33 +0000

    adiciona scripts `link:check`e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1> Eu amo você, jow! </h1>
+  return <h1> Eu amo você, jow! </h1>;
 }
 
+function teste() {
+  console.log("Formatação errada");
+}
 export default Home;
